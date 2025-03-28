9cc2fa1b2b5f17f0050b215d5e5241ae5a83ee16
sunilrudrakumar <sunilrudrakumar@gmail.com>
2025-03-28 17:22:38 -0400

    Updating the ui

--- frontend/src/components/main.tsx
+++ frontend/src/components/main.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import { Github, Cloud, Database, Box, Cpu, CloudLightning as Lightning, ArrowRight, Terminal } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
